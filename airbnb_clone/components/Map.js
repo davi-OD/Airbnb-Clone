7bba91a9f31122afae5a9374e129c48668433998
davi-OD <davidodie51@gmail.com>
2021-08-10 23:30:49 +0300

    updated Map now adjustable

--- airbnb_clone/components/Map.js
+++ airbnb_clone/components/Map.js
@@ -1,15 +1,16 @@
 import { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, {Marker, Popup} from 'react-map-gl';
 import getCenter from "geolib/es/getCenter";
 
 
 function Map({ searchResults }) {
+    const [selectedLocation, setSelectedLocation] = useState({})
 
     //Transform the search result object into the {latitude:..., longitude:....} object
     const coordinates = searchResults.map((result) => ({
         longitude: result.long,
         latitude: result.lat,
-    }))
+    }));
     // The latitude and longitude if the center of locations coordinates
     const center = getCenter(coordinates);
 
@@ -17,18 +18,33 @@ function Map({ searchResults }) {
         width: '100%',
         height: '100%',
         latitude: center.latitude,
-        longitude: center.latitude,
+        longitude: center.longitude,
         zoom: 11,
     });
 
 
     return (
         <ReactMapGL 
-        mapStyle="mapbox://styles/davi-od/cks5q4gs57dw217qoti7dwtz9"
+            mapStyle="mapbox://styles/davi-od/cks5q4gs57dw217qoti7dwtz9"
             mapboxApiAccessToken={process.env.mapbox_key}
             {...viewport}
-            onViewChange={(nextViewport) => setViewport(nextViewport)}
-        ></ReactMapGL>      
+            onViewportChange={(nextViewport) => setViewport(nextViewport)}
+        >
+
+            {searchResults.map((result) => (
+                <div key={result.long}>
+                    <Marker 
+                        longitude={result.long}
+                        latitude={result.lat}
+                        offsetLeft={-20}
+                        offsetTop={-10}
+                    >
+                        <p className="cursor-pointer text-2xl animate-bounce">📌</p>
+                    </Marker>
+                </div>
+            ))};
+
+        </ReactMapGL>      
     );  
 }
 
